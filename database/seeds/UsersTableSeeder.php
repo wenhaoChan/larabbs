0961efbeaa39920155c29acde43ff067078852a5
chenwenhao <zerochan1220@163.com>
2018-07-23 11:58:00 +0800

    Integrate laravel-permission pakege

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -45,6 +45,12 @@ class UsersTableSeeder extends Seeder
         $user->name = 'zero';
         $user->email = 'zerochan1220@163.com';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
         $user->save();
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
