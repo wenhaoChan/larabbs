e8d82d7cdf8f32277250f1699d8b6b614de1d385
chenwenhao <zerochan1220@163.com>
2018-07-23 18:37:42 +0800

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="https://weibo.com/u/1997577791/home" target="_blank">zero</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
